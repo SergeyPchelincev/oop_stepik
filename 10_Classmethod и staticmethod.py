b85bd161b2b2b52e9b98688d463207be583a94ad
VladimirLebedev <name_mercy@mail.ru>
2023-06-19 22:37:17 +0300

    add info getter, setter classmethod, staticmethod

--- 10_Classmethod и staticmethod.py
+++ 10_Classmethod и staticmethod.py
@@ -65,4 +65,35 @@
 # print(robot.make_clean())
 
 
+class Circle:
+
+    def __init__(self, radius):
+        if not Circle.is_positive(radius):
+            raise ValueError("Радиус должен быть положительным")
+        self.radius = radius
+
+    @classmethod
+    def from_diameter(cls, diameter):
+        new_circle_cls = cls(diameter/2)
+        return new_circle_cls
+
+    @staticmethod
+    def is_positive(number):
+        if number > 0:
+            return True
+
+    @staticmethod
+    def area(radius_v2):
+        return 3.14 * radius_v2 ** 2
+
+# код ниже не нужно удалять, в нем находятся проверки
+circle_1 = Circle.from_diameter(10)
+assert isinstance(circle_1, Circle)
+assert circle_1.radius == 5.0
+print(f"circle_1.radius={circle_1.radius}")
+assert Circle.is_positive(10)
+assert not Circle.is_positive(-5)
+assert Circle.area(1) == 3.14
+assert Circle.area(2) == 12.56
+
 
